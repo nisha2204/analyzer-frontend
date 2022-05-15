b994f4c6283a4139df48d7dab4f27e668239ab49
nisha2204 <modaninisha@gmail.com>
2022-05-15 14:20:23 +0530

    --

--- src/components/Register.js
+++ src/components/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
     const Register = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/users', {
+            await axios.post('https://listanalyzer.herokuapp.com/users', {
                 name: name,
                 email: email,
             });
